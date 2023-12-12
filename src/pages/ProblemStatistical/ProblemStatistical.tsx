af8df264bf9e051da339e3c1b6fd52ea85c5e2ac
Nguyễn Hữu  An <an.nguyen@risensgroup.com>
2023-12-12 22:52:22 +0700

    add package and remove file helper

---
+++ src/pages/ProblemStatistical/ProblemStatistical.tsx
@@ -0,0 +1,19 @@
+import {
+  CategoryScale,
+  Chart,
+  LineElement,
+  LinearScale,
+  PointElement,
+} from "chart.js";
+import "./dashboardItem.scss";
+
+Chart.register(CategoryScale);
+Chart.register(LinearScale);
+Chart.register(PointElement);
+Chart.register(LineElement);
+
+const ProblemStatistical = () => {
+  return <></>;
+};
+
+export default ProblemStatistical;
